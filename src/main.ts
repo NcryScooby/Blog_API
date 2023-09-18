a0de9dc359c3ac008e944acc70d4678a6a8bac80
Fabrício <fabricio_c_anelli@hotmail.com>
2023-09-18 20:12:00 -0300

    revert: vercel deploy

--- src/main.ts
+++ src/main.ts
@@ -3,28 +3,18 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from '@src/app.module';
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
 import { env } from '@config/env';
 import * as path from 'path';
-import * as express from 'express';
 
-const server = express();
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-async function createNestApp() {
-  const nestApp = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-    new ExpressAdapter(server),
-  );
-
-  nestApp.useGlobalPipes(new ValidationPipe());
-  nestApp.useStaticAssets(path.join(__dirname, '..', './'));
-  nestApp.enableCors({
+  app.useGlobalPipes(new ValidationPipe());
+  app.useStaticAssets(path.join(__dirname, '..', './'));
+  app.enableCors({
     origin: env.clientUrl,
   });
 
-  await nestApp.init();
+  await app.listen(3001);
 }
-
-createNestApp();
-
-export = server;
+bootstrap();
